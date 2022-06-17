5c4b7477387be4271be5dad27fdc141444c517b5
Juan Rodríguez Pérez <juanrdg10@gmail.com>
2022-06-17 21:19:52 +0200

    Basic random array creation
    The values at each lattice point can be either 0 or 1

---
+++ creation.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+# Creation of the array of ones and zeroes representing the spin up or spin down state of each lattice point
+
+def create_random_distribution(N): # Here N is the number of rows/columns in the lattice. N^2 is the number of lattice points
+    arr = np.random.rand(N,N)
+    
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i,j]<0.5:
+                arr[i,j] = 0
+            else:
+                arr[i,j] = 1
+
+    return arr
